42495487ff7193b158a1529245049ac913336615
Sebastian <105529161+Basttuu@users.noreply.github.com>
2025-11-21 16:44:13 -0500

    update port
    ee

--- main.py
+++ main.py
@@ -296,4 +296,4 @@ def maestro_general():
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
