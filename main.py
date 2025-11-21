caa56792331e6f499df55d9647145b4965fdee68
Sebastian <105529161+Basttuu@users.noreply.github.com>
2025-11-21 16:19:03 -0500

    mm

--- main.py
+++ main.py
@@ -292,3 +292,8 @@ def maestro_general():
 
     html += "</body></html>"
     return HTMLResponse(html)
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
