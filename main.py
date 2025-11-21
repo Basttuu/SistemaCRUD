a74290b93d47e53265452ebb515da5d456667251
Sebastian <105529161+Basttuu@users.noreply.github.com>
2025-11-21 16:56:21 -0500

    mm
    mm

--- main.py
+++ main.py
@@ -294,6 +294,8 @@ def maestro_general():
     return HTMLResponse(html)
 
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
